93a3c752816beefe7e75e4c87d622b6fdb3c2e0d
polsan2 <petrojr.mail@gmail.com>
2025-02-01 22:19:04 +0000

    first compile and sent to device

--- arduino_nfc/nfc.ino
+++ arduino_nfc/nfc.ino
@@ -3,33 +3,31 @@
 #include <PN532.h>
 #include <NfcAdapter.h>
 
-#define NDEF_USE_SERIAL
+// Define custom I2C pins
+#define SDA_PIN 10
+#define SCL_PIN 11
 
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
-String tagId = "2424";
-byte nuidPICC[4];
- 
-void setup(void) 
-{
-  Wire.begin(10, 11); // Custom I2C pins (SDA=GPI10, SCL=GPI11 for ESP32/RP2040)
-  Serial.begin(115200);
-  Serial.println("System initialized");
-  nfc.begin();
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial); // Wait for Serial Monitor to open
+
+    // Set custom I2C pins for RP2040
+    Wire.setSDA(SDA_PIN);
+    Wire.setSCL(SCL_PIN);
+    Wire.begin();
+
+    Serial.println("Initializing NFC Module...");
+    nfc.begin();
 }
- 
-void loop() 
-{
- readNFC();
-}
- 
-void readNFC() 
-{
- if (nfc.tagPresent())
- {
-   NfcTag tag = nfc.read();
-   tag.print();
-   tagId = tag.getUidString();
- }
- delay(5000);
+
+void loop() {
+    if (nfc.tagPresent()) {
+        NfcTag tag = nfc.read();
+        Serial.println("NFC Tag Detected!");
+        tag.print();
+    }
+    delay(5000);
 }
