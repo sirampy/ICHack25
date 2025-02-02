29ccee029f7c30aa9ebef8d3e411b75771914178
polsan2 <petrojr.mail@gmail.com>
2025-02-02 06:37:59 +0000

    api for network working

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,9 @@
 use actix_files as fs;
-use actix_web::{cookie::{Cookie, SameSite}, get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    cookie::{Cookie, SameSite},
+    get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use actix_cors::Cors;
 use serde::Deserialize;
 mod api;
 use std::sync::RwLock;
@@ -10,68 +14,66 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-
 async fn index_get(req: HttpRequest) -> impl Responder {
-
-    match req.cookie("username"){
+    match req.cookie("username") {
         Some(username) => HttpResponse::Found().header("Location", "/home").finish(),
-        None => HttpResponse::Found().header("Location", "/register").finish()
-    } 
-     
+        None => HttpResponse::Found().header("Location", "/register").finish(),
+    }
 }
 
 #[derive(Template)]
-#[template(path = "home.html")] 
+#[template(path = "home.html")]
 struct Home_template {}
-
 
 async fn home_get() -> impl Responder {
     Home_template {}
 }
 
-
 #[derive(Template)]
-#[template(path = "collector.html")] 
+#[template(path = "collector.html")]
 struct Collector_template {}
-
 
 async fn collector_get() -> impl Responder {
     Collector_template {}
 }
 
-
 #[derive(Template)]
-#[template(path = "register.html")] 
+#[template(path = "register.html")]
 struct RegisterTemplate {}
 
 async fn register_get() -> impl Responder {
     RegisterTemplate {}
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    
-    let data = web::Data::new(RwLock::new(api::ApiState::fakedata())); 
-        
+    let data = web::Data::new(RwLock::new(api::ApiState::fakedata()));
+
     HttpServer::new(move || {
         App::new()
+            // Wrap your app with CORS middleware
+            .wrap(
+                Cors::default()
+                    // For testing, allow any origin. For production, you might restrict this.
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header()
+                    .max_age(3600),
+            )
             .service(
                 web::scope("/api")
-                .app_data(data.clone())
-                .service(api::hello)
-                .service(api::upload_edges)
-                )
+                    .app_data(data.clone())
+                    .service(api::hello)
+                    .service(api::upload_edges)
+            )
             .service(echo)
             .route("/", web::get().to(index_get))
             .route("/home", web::get().to(home_get))
             .route("/register", web::get().to(register_get))
             .route("/collector", web::get().to(collector_get))
-
             .service(fs::Files::new("/static", "static/").show_files_listing())
     })
     .bind(("127.0.0.1", 8080))?
